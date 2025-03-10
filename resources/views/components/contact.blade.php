bc535eee1f283d5cc342b60d6f14ded0321c9a0d
aliather392 <118840507+aliather392@users.noreply.github.com>
2025-03-10 11:42:31 +0500

    Update contact.blade.php

--- resources/views/components/contact.blade.php
+++ resources/views/components/contact.blade.php
@@ -173,8 +173,7 @@ style="box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 
         </div>
     </div>
 </div>
-
-<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" ></script>
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback" defer></script>
 
 <script>
     $(document).ready(function(){
